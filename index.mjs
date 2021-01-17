0f6a095428ee023f518a3a4ad7b046df299739a2
codeeno <60033298+codeeno@users.noreply.github.com>
2021-01-17 22:34:55 +0100

    add arguments parsing

--- index.mjs
+++ index.mjs
@@ -1,29 +1,57 @@
+#!/usr/bin/env node
+import yargs from "yargs";
+import { hideBin } from "yargs/helpers";
 import Configstore from "configstore";
-import { addProfile, chooseProfile } from "./lib/profiles.mjs";
-import { refreshToken, refreshClient, clientIsValid, tokenIsValid } from "./lib/auth.mjs";
+import { addProfile, chooseProfile, loadConfig } from "./lib/profiles.mjs";
+import { refreshCredentials } from "./lib/auth.mjs";
+import { handleError } from "./lib/error.mjs";
+import { chooseAccount, chooseRole } from "./lib/accounts.mjs";
 
-const config = new Configstore("aws-sso-switcher");
+const configstore = new Configstore("aws-sso-cli");
 
-const isEmptyConfig = (config) => !config || !Object.keys(config.all).length;
-const checkConfigValidity = async () => {
-  if (!clientIsValid(config, profile)) {
-    await refreshClient(config, profile);
-    await refreshToken(config, profile);
+const signInHandler = async (argv) => {
+  try {
+    const profile = "profile" in argv ? argv.profile : await chooseProfile(configstore);
+    await refreshCredentials(configstore, profile);
+    const {token: { accessToken }} = loadConfig(configstore, profile); //prettier-ignore
+
+    const accounts = await chooseAccount(accessToken);
+    console.log(accounts);
+    //const accounts = await chooseRole(config, profile);
+    //console.log(roles);
+  } catch (err) {
+    handleError(err);
   }
 
-  if (!tokenIsValid(config, profile)) {
-    await refreshToken(config, profile);
+  console.log("all done");
+  process.exit(0);
+};
+
+const addProfileHandler = async () => {
+  try {
+    await addProfile(config);
+    console.log("All set!");
+  } catch (err) {
+    console.error("Error while addind new profile:", err);
   }
 };
 
-if (isEmptyConfig(config)) {
-  console.log("Looks like you haven't set up any profiles yet. Let's create one first.");
-  await addProfile(config);
-  process.exit(0);
-}
-
-const profile = await chooseProfile(config);
-await checkConfigValidity();
-
-console.log("all done");
-process.exit(0);
+yargs(hideBin(process.argv))
+  .scriptName("aws-sso-cli")
+  .usage("Usage: $0 [options]")
+  .command({
+    command: "add-profile",
+    desc: "Add a new SSO profile",
+    handler: addProfileHandler,
+  })
+  .command({
+    command: "$0",
+    desc: "Sign in to an AWS account using AWS SSO",
+    handler: signInHandler,
+  })
+  .option("p", {
+    alias: "profile",
+    describe: "The SSO profile to use.",
+    type: "string",
+  })
+  .help("help", "Show help.").argv;
